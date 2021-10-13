1b39261a36e171051ce05a001cbe2be3e1c65ed9
hwangseunghwan <zen896@gmail.com>
2021-10-13 16:41:19 +0900

    django course 31 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -31,16 +31,19 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
