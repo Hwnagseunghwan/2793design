a9e3c17816b055fb40f5124c5219658f0825f2fa
hwangseunghwan <zen896@gmail.com>
2021-11-22 18:31:02 +0900

    django course 43 commit

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -18,10 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from articleapp.views import ArticleListView
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='home'),
 
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
@@ -29,7 +27,6 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
-    #path('subscribe/', include('subscribeapp.urls')),
-    #path('likes/', include('likeapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
