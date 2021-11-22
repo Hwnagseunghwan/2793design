8f1ac42355cc29cf6568ebe431aedf5445ed2602
hwangseunghwan <zen896@gmail.com>
2021-11-22 21:08:48 +0900

    djanago course 44 commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
+                                                           'style': 'height: auto;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all()) #(<-, required=False) Project 목록에서 항목을 선택하지 않아도 되는 옵션
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
